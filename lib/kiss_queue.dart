b8197cdce81626c6a2348a73e47dafe8fb7d4f1e
Lee Higgins <lee@wearemobilefirst.com>
2025-06-18 14:52:34 +0200

    move tests to own folder

--- lib/kiss_queue.dart
+++ lib/kiss_queue.dart
@@ -3,6 +3,6 @@ library;
 export 'src/queue.dart';
 export 'src/in_memory_queue.dart';
 
-export 'src/implementation_tester.dart';
-export 'src/queue_test_suite.dart';
-export 'src/test_models.dart';
+export 'src/test/implementation_tester.dart';
+export 'src/test/queue_test_suite.dart';
+export 'src/test/test_models.dart';
